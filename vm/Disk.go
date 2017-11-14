ea1b5ad03cef08b2935815599250d61ab5399058
Gavin Smith <gavinasmith@me.com>
2017-11-13 22:02:56 -0500

    Cleanup

--- vm/Disk.go
+++ vm/Disk.go
@@ -21,48 +21,6 @@ func MakeDisk() Disk {
 	return d
 }
 
-// AddressFetchWord returns the word value at the given address.
-func (d Disk) AddressFetchWord(addr ivm.Address) ivm.Word {
-	framenum, frameaddr := ivm.FrameComponentsForAddress(addr / 4)
-	return d[framenum][frameaddr]
-}
-
-// AddressWriteWord writes the given word value to the given address.
-func (d *Disk) AddressWriteWord(addr ivm.Address, val ivm.Word) {
-	framenum, frameaddr := ivm.FrameComponentsForAddress(addr / 4)
-	d[framenum][frameaddr] = val
-}
-
-// AddressFetchUint32 returns the uint32 value at the given address.
-func (d Disk) AddressFetchUint32(addr ivm.Address) uint32 {
-	return d.AddressFetchWord(addr).Uint32()
-}
-
-// AddressWriteUint32 writes the given uint32 value to the given address.
-func (d *Disk) AddressWriteUint32(addr ivm.Address, val uint32) {
-	d.AddressWriteWord(addr, ivm.WordFromUint32(val))
-}
-
-// AddressFetchInt32 returns the int32 value at the given address.
-func (d Disk) AddressFetchInt32(addr ivm.Address) int32 {
-	return d.AddressFetchWord(addr).Int32()
-}
-
-// AddressWriteInt32 writes the given int32 value to the given address.
-func (d *Disk) AddressWriteInt32(addr ivm.Address, val int32) {
-	d.AddressWriteWord(addr, ivm.WordFromInt32(val))
-}
-
-// AddressFetchBool returns the bool value at the given address.
-func (d Disk) AddressFetchBool(addr ivm.Address) bool {
-	return d.AddressFetchWord(addr).Bool()
-}
-
-// AddressWriteBool writes the given bool value to the given address.
-func (d *Disk) AddressWriteBool(addr ivm.Address, val bool) {
-	d.AddressWriteWord(addr, ivm.WordFromBool(val))
-}
-
 // FrameFetch fetches the frame with the given frame number.
 func (d Disk) FrameFetch(frameNum ivm.FrameNumber) ivm.Frame {
 	return d[frameNum]
