78611fd45af0afa68e16e2132aa62f421d3b3a53
Gavin Smith <gavinasmith@me.com>
2017-11-19 20:24:27 -0500

    some other changes

--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 
 	"./config"
 	"./vm"
+	// "fmt"
 	// "./disp"
-	"fmt"
 )
 
 func main() {
 	var err error
 	var sharedConfig config.Config
-	var virtualMachine vm.VM
+	var virtualMachine *vm.VM
 
 	// load app configuration
 	sharedConfig, err = config.Shared()
@@ -26,15 +26,26 @@ func main() {
 	log.Printf("program file: %s\n", sharedConfig.Progfile)
 
 	// build the virtual machine with the given config
-	virtualMachine, err = vm.MakeVM(sharedConfig)
+	virtualMachine, err = vm.New(sharedConfig)
 	if err != nil {
 		log.Fatalf("error building virtual machine: %v", err)
 		return
 	}
 
-	fmt.Print("RAM Dump:\n")
-	_ = virtualMachine.RAM.Print()
-	fmt.Print("\n")
+	pChan := virtualMachine.Run()
+
+	for {
+		p := <-pChan
+		log.Printf("[%3d%%]: %s", int(p.Value*100), p.Title)
+		if p.Value == 1.0 {
+			break
+		}
+	}
+	log.Printf("done!\n")
+
+	// fmt.Print("RAM Dump:\n")
+	// _ = virtualMachine.RAM.Print()
+	// fmt.Print("\n")
 
 	// fmt.Print("Disk Dump:\n")
 	// _ = virtualMachine.Disk.Print()
