7a1f043e2ac8feee5982a00921d1c557fab68da1
Gavin Smith <gavinasmith@me.com>
2017-12-02 22:14:12 -0500

    fixed logic and formatting for MOV

--- vm/instr/MOV.go
+++ vm/instr/MOV.go
@@ -14,13 +14,13 @@ type MOV struct {
 // Execute runs the given MOV instruction
 func (i MOV) Execute(ip ivm.InstructionProxy) {
 	source := ip.RegisterWord(i.args.Source2)
-	ip.SetRegisterWord(i.args.Destination, source)
+	ip.SetRegisterWord(i.args.Source1, source)
 }
 
 // Assembly returns the representation in assembly language
 func (i MOV) Assembly() string {
 	// return fmt.Sprintf("MOV %s", i.args.ASM())
-	return fmt.Sprintf("MOV %s %s", i.args.Destination, i.args.Source2)
+	return fmt.Sprintf("MOV %s %s", i.args.Source1, i.args.Source2)
 }
 
 // MakeMOV makes an MOV instruction for the given args
